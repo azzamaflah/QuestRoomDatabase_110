7f71eaa9b9faaed621ea2c764fed7676e812a0cb
azzamaflah <azzamhebat46@gmail.com>
2024-12-16 02:29:11 +0700

    Menambahkan inisialisasi HomeMhsViewModel, DetailMhsViewModel, dan UpdateMhsViewModel dengan SavedStateHandle dan repository mahasiswa.

--- app/src/main/java/com/example/pammmm/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pammmm/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.pammmm.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.Factory
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -11,6 +12,25 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
