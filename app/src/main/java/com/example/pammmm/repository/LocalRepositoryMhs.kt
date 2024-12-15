922f6402ec4520d05c4b5eef4ed6a21d709d7409
azzamaflah <azzamhebat46@gmail.com>
2024-12-16 00:30:44 +0700

    untuk menambahkan crud nya

--- app/src/main/java/com/example/pammmm/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pammmm/repository/LocalRepositoryMhs.kt
@@ -5,13 +5,28 @@ import com.example.pammmm.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
+
+
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
+
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+
     }
 
     override fun getAllMahasiswa(): Flow<List<Mahasiswa>> {
         return mahasiswaDao.getAllMahasiswa()
+
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa>{
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+    override suspend fun updateMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
