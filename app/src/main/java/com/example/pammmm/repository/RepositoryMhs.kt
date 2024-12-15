299d8e21ebaf3c231d9b8e65ce5ea8e5e5199370
azzamaflah <azzamhebat46@gmail.com>
2024-12-16 00:32:38 +0700

    untuk menambahkan crud dan mengambil data mahasiswa berdasarkan nim, delet data sama update data nya

--- app/src/main/java/com/example/pammmm/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pammmm/repository/RepositoryMhs.kt
@@ -7,7 +7,7 @@ interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
     fun getMhs(nim: String): Flow<Mahasiswa>
-    suspend fun  deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
     suspend fun updateMhs(mahasiswa: Mahasiswa)
 
 }
