554ea3895c46fa080159a87155d3df0f41fe1bd0
azzamaflah <azzamhebat46@gmail.com>
2024-12-16 00:07:03 +0700

    untuk updateMahasiswa dari mahasiswa dao memperbarui data mahasiswa di database

--- app/src/main/java/com/example/pammmm/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pammmm/repository/RepositoryMhs.kt
@@ -8,7 +8,6 @@ interface RepositoryMhs {
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
     fun getMhs(nim: String): Flow<Mahasiswa>
     suspend fun  deleteMhs(mahasiswa: Mahasiswa)
-
-
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 
 }
